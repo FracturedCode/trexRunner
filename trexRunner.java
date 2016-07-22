61cedf97cc1622c79c06e4060de6b2b5ca5feabc
Gabe C <coding1729@gmail.com>
2016-07-22 01:34:59 -0700

    Completed

--- trexRunner.java
+++ trexRunner.java
@@ -11,6 +11,7 @@ import javafx.scene.text.FontWeight;
 import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
+import java.util.ArrayList;
 
 public class trexRunner extends Application {
 	public static void main(String[] args) {
@@ -19,51 +20,90 @@ public class trexRunner extends Application {
 
 	@Override
 	public void start(Stage gameStage) {
-		gameStage.setTitle("T-Rex Runner");
+	    gameStage.setTitle("T-Rex Runner");
+	    
+	    Group root = new Group();        
+	    Scene gameScene = new Scene(root); 
+	    gameStage.setScene(gameScene);     
 
-		Group root = new Group();
-		Scene gameScene = new Scene(root);
-		gameStage.setScene(gameScene);
+	    Canvas gameCanvas = new Canvas(800, 230);
+	    root.getChildren().add(gameCanvas);
 
-		Canvas gameCanvas = new Canvas(800, 230);
-		root.getChildren().add(gameCanvas);
+	    GraphicsContext gc = gameCanvas.getGraphicsContext2D();      
+	    String ground = "________________________________________________________________";
+	    gc.setFill(Color.BLACK);    
+	    gc.setLineWidth(2);                        
+	    Font gameFont = Font.font("Times New Roman", FontWeight.LIGHT, 20);
+	    gc.setFont(gameFont);        
 
-		String ground = "________________________________________________________________";
+	    Image trex = new Image("trex.png");  
+	    Image cactus = new Image("cactus.png");
 
-		GraphicsContext gc = gameCanvas.getGraphicsContext2D();
+	    ArrayList<String> input = new ArrayList<String>();
 
-		gc.setFill(Color.BLACK);
-		gc.setLineWidth(2);
-		Font gameFont = Font.font("Times New Roman", FontWeight.LIGHT, 20);
-		gc.setFont(gameFont);
-		gc.fillText(ground, 80, 160);
+	    gameScene.setOnKeyPressed(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent e)
+                {
+                    String code = e.getCode().toString();
+                    if ( !input.contains(code) )
+                        input.add( code );
+                }
+            });
+ 
+        gameScene.setOnKeyReleased(
+            new EventHandler<KeyEvent>()
+            {
+                public void handle(KeyEvent e)
+                {
+                    String code = e.getCode().toString();
+                    input.remove( code );
+                }
+            });
 
-		Image trex = new Image("trex.png");
-		Image cactus = new Image("cactus.png");
-		gc.drawImage(trex, 80, 100);
-		gc.drawImage(cactus, 800, 100);
+	    new AnimationTimer() {
+		    long lastNanoTime = System.nanoTime();
+		    int cactusX = 800;
+		    int trexY = 100;
+		    boolean trexPeak = false;
+		    public void handle(long currentNanoTime) {
+		        long t = (currentNanoTime - lastNanoTime);
+		        if (t >= 16666666) {
+		            while (t >= 16666666) {
 
-		new AnimationTimer() {
-			long lastNanoTime = System.nanoTime();
-			int cactusX = 800;
-			public void handle(long currentNanoTime) {
-				long t = (currentNanoTime - lastNanoTime);
-				while (t >= 10000000) {
-					gc.clearRect(0, 0, 800, 230);
-					gc.fillText(ground, 80, 160);
-					gc.drawImage(trex, 80, 100);
-					if (cactusX <= 0) {
-						cactusX = 800;
-					} else {
-						cactusX = cactusX - 5;
-					}
-					gc.drawImage(cactus, cactusX, 110);
-					lastNanoTime = System.nanoTime();
-					t -= 16666666;
-				}
-			}
+		            	if (input.contains("SPACE") || input.contains("UP")) {
+		            		if (!trexPeak && trexY>5) {
+		            			trexY -= 5;
+		            		} else if (!trexPeak && trexY<=5) {
+		            			trexPeak = true;
+		            		} else if (trexPeak && trexY<100) {
+		            			trexY += 5;
+		            		} else if (trexPeak && trexY>=100) {
+		            			trexPeak = false;
+		            		} else {
+		            			System.out.println("Error!");
+		            		}
+		            	} else {
+		            		trexY = 100;
+		            	}
+
+		                if (cactusX <= 30) {
+					        cactusX = 700;
+					    } else {
+					        cactusX -= 5;
+					    }
+					    t -= 16666666;
+		            }
+		            gc.clearRect(0, 0, 800, 230);
+		            gc.fillText(ground, 80, 160);
+			    	gc.drawImage(trex, 80, trexY);
+		            gc.drawImage(cactus, cactusX, 110);
+		            lastNanoTime = System.nanoTime();
+		        }
+		    }
 		}.start();
 
-		gameStage.show();
+	    gameStage.show();
 	}
 }
